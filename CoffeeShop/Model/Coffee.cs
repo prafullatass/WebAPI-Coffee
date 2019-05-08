06f4612f2519798ec5e53bb9fe2aedec21afe7ae
Prafullata Sonawane <prafullatass@gmail.com>
2019-05-08 15:54:23 -0500

    First Commit

---
+++ CoffeeShop/Model/Coffee.cs
@@ -0,0 +1,9 @@
+﻿namespace CoffeeShop.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string BeanType { get; set; }
+    }
+}
